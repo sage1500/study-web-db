18679103096f9cc4c9ab22ffcd3c125d6ea9e592
sage1500 <motosage25@gmail.com>
2022-04-17 00:51:48 +0900

    不要なログ削除

--- demo-common/src/main/java/com/example/demo/common/error/CommonErrorAttributes.java
+++ demo-common/src/main/java/com/example/demo/common/error/CommonErrorAttributes.java
@@ -9,22 +9,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.WebRequest;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Component
-@Slf4j
 public class CommonErrorAttributes extends DefaultErrorAttributes {
 
 	@Override
 	public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
-		log.info("getErrorAttributes");
 		var attrs = super.getErrorAttributes(webRequest, options);
 		return attrs;
 	}
 
 	@Override
 	public Throwable getError(WebRequest webRequest) {
-		log.info("getError");
 		var exception = super.getError(webRequest);
 		if (exception == null) {
 			exception = getAttribute(webRequest, WebAttributes.AUTHENTICATION_EXCEPTION);
--- demo-common/src/main/java/com/example/demo/common/error/CommonErrorViewResolver.java
+++ demo-common/src/main/java/com/example/demo/common/error/CommonErrorViewResolver.java
@@ -8,43 +8,39 @@ import org.springframework.boot.autoconfigure.template.TemplateAvailabilityProvi
 import org.springframework.boot.autoconfigure.template.TemplateAvailabilityProviders;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Component
-@Order(Integer.MAX_VALUE - 1)
-@Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
 public class CommonErrorViewResolver implements ErrorViewResolver {
-	
+
 	private final ApplicationContext applicationContext;
-	private final TemplateAvailabilityProviders templateAvailabilityProviders;	
+	private final TemplateAvailabilityProviders templateAvailabilityProviders;
 
 	public CommonErrorViewResolver(ApplicationContext applicationContext) {
 		this.applicationContext = applicationContext;
 		this.templateAvailabilityProviders = new TemplateAvailabilityProviders(applicationContext);
 	}
-	
+
 	/**
 	 * 
 	 */
 	@Override
 	public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
 		var type = request.getParameter("type");
-		log.info("[!] type={}", type);
-		
+
+		// TODO セキュリティホールにならないように、実際には有効な type の判定が必要
 		String errorViewName = "error/" + type;
 		TemplateAvailabilityProvider provider = this.templateAvailabilityProviders.getProvider(errorViewName,
 				this.applicationContext);
 		if (provider != null) {
-			log.info("[!] has-view");
 			return new ModelAndView(errorViewName, model);
-		}		
-		
-		log.info("[!] no-view");
+		}
+
 		return null;
 	}
 
