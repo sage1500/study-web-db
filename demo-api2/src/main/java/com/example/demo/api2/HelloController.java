6b201b275556b6da376a034634bd0761fb108863
sage1500 <motosage25@gmail.com>
2022-04-26 00:53:55 +0900

    公開鍵指定でのJWT検証

--- demo-api2/src/main/java/com/example/demo/api2/HelloController.java
+++ demo-api2/src/main/java/com/example/demo/api2/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo.api2;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,20 @@ import lombok.extern.slf4j.Slf4j;
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String hello() {
-		log.info("[HELLO] hello");
+	public String hello(@AuthenticationPrincipal Jwt jwt) {
+		log.info("[HELLO] hello: {}", jwt);
+		if (jwt != null) {
+			log.info("  user: {}", jwt.getClaimAsString("preferred_username"));
+		}
 		return "hello";
 	}
 
 	@GetMapping("hello2")
-	public String hello2() {
-		log.info("[HELLO] hello2");
+	public String hello2(@AuthenticationPrincipal Jwt jwt) {
+		log.info("[HELLO] hello2: {}", jwt);
+		if (jwt != null) {
+			log.info("  user: {}", jwt.getClaimAsString("preferred_username"));
+		}
 		return "hello2";
 	}
 
